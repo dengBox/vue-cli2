5f0d943c694682e2ffa45eab7161637871499e57
dengbo <1716775221@qq.com>
2019-11-29 14:48:27 +0800

    增加使用富文本编辑器示例

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,91 +1,81 @@
 <template>
-  <div class="test-wrap">
-    <ul>
-      <li>
-        <h4>汉字转拼音</h4>
-        <Input v-model="inputVal" placeholder="请输入"><div slot="prepend">输入：</div></Input>
-        <br />
-        <Input v-model="output" readonly ><div slot="prepend">输出：</div></Input>
-        <br />
-        <Input v-model="outputFirst" readonly ><div slot="prepend">输出：</div></Input>
-      </li>
-      <li>
-        <h4>基本搜索</h4>
-        <Input :value="searchArr.join('、')" readonly ><div slot="prepend">选项：</div></Input>
-        <br />
-        <Input v-model="defaultVal" placeholder="模糊搜索"><div slot="prepend">输入：</div></Input>
-        <br />
-        <Input v-model="outputDef" readonly ><div slot="prepend">输出：</div></Input>
-      </li>
-      <li>
-        <h4>拼音搜索</h4>
-        <Input :value="searchArr.join('、')" readonly ><div slot="prepend">选项：</div></Input>
-        <br />
-        <Input v-model="sinVal" placeholder="支持拼音"><div slot="prepend">输入：</div></Input>
-        <br />
-        <Input v-model="outputSin" readonly ><div slot="prepend">输出：</div></Input>
-      </li>
-    </ul>
-  </div>
+<div>
+  <!--
+    给editor加key是因为给tinymce keep-alive以后组件切换时tinymce编辑器会显示异常，
+    在activated钩子里改变key的值可以让编辑器重新创建
+  -->
+  <editor id="tinymceEditor" :init="tinymceInit" v-model="content" :key="tinymceFlag"></editor>
+</div>
 </template>
-
 <script>
-import sinPinyin from '../assets/js/sinToPinyin'
-import { DefaultSearch, SinSearch } from '../assets/js/search'
+import tinymce from 'tinymce/tinymce'
+import 'tinymce/themes/silver/theme'
+import Editor from '@tinymce/tinymce-vue'
+
+import 'tinymce/plugins/textcolor'
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/paste'
+import 'tinymce/plugins/preview'
+import 'tinymce/plugins/fullscreen'
+
 export default {
-  name: 'HelloWorld',
+  name: 'TinymceEditor',
+  components: {
+    'editor': Editor
+  },
   data () {
     return {
-      inputVal: '',
-      output: '',
-      outputFirst: '',
-      defaultVal: '',
-      outputDef: '',
-      sinVal: '',
-      outputSin: '',
-      searchArr: [
-        '语文',
-        'yuwen',
-        'YuWen',
-        '数学',
-        'NBA',
-        'nba',
-        1,
-        2,
-        '12321'
-      ],
-      searchDef: {},
-      searchSin: {}
-    }
-  },
-  watch: {
-    inputVal (val) {
-      this.output = sinPinyin(val)
-      this.outputFirst = sinPinyin(val, true)
-    },
-    defaultVal (val) {
-      this.outputDef = this.searchDef.search(val).join('、')
-    },
-    sinVal (val) {
-      this.outputSin = this.searchSin.search(val).join('、')
+      tinymceFlag: 1,
+      tinymceInit: {},
+      content: '本地图片上传功能仅为演示，实际使用需要补全图片存储地址'
     }
   },
   methods: {
+    // 插入图片至编辑器
+    insertImage (res, file) {
+      let src = '' // 图片存储地址
+      tinymce.execCommand('mceInsertContent', false, `<img src=${src}>`)
+    }
   },
   created () {
-    this.searchDef = new DefaultSearch(this.searchArr)
-    this.searchSin = new SinSearch(this.searchArr)
+    const that = this
+    this.tinymceInit = {
+      skin_url: 'static/tinymce/skins/ui/oxide',
+      language_url: `static/tinymce/langs/zh_CN.js`,
+      language: 'zh_CN',
+      height: document.body.offsetHeight - 300,
+      browser_spellcheck: true, // 拼写检查
+      branding: false, // 去水印
+      // elementpath: false,  //禁用编辑器底部的状态栏
+      statusbar: false, // 隐藏编辑器底部的状态栏
+      paste_data_images: true, // 允许粘贴图像
+      menubar: false, // 隐藏最上方menu
+      plugins: 'advlist table lists paste preview fullscreen',
+      toolbar: 'fontselect fontsizeselect forecolor backcolor bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | imageUpload quicklink h2 h3 blockquote table numlist bullist preview fullscreen',
+      /**
+       * 下面方法是为tinymce添加自定义插入图片按钮
+       * 借助iview的Upload组件,将图片先上传到存储云上，再将图片的存储地址放入编辑器内容
+       */
+      setup: (editor) => {
+        editor.ui.registry.addButton('imageUpload', {
+          // text: '插入图片',
+          tooltip: '插入图片',
+          icon: 'image',
+          onAction: () => {
+            let upload = that.$refs.imageUpload
+            upload.handleClick()
+          }
+        })
+      }
+    }
+  },
+  activated () {
+    this.tinymceFlag++
+  },
+  mounted () {
+
   }
 }
 </script>
-<style>
-.test-wrap {
-  width: 400px;
-  margin: 40px auto;
-}
-
-h4 {
-  padding: 10px 0;
-}
-
-</style>
